7bef778cc3eb92ef16cbb60601d9c61c32907a52
Alphy Joy <contact.alphyjoy@gmail.com>
2022-04-02 15:45:28 +0530

    Completed blog listing and front end integration

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -7,24 +7,35 @@
         </h1>
     </div>
 </div>
+@if (Auth::check())
+    <div class="pt-15 w-4/5 m-auto">
+        <a href="/blog/create" class="bg-blue-500 uppercase bg-transparent text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+            Create Post
+        </a>
+    </div>    
+@endif
+@foreach ($posts as $post )
 <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
     <div>
         <img src="https://cdn.pixabay.com/photo/2016/03/26/13/09/workspace-1280538_960_720.jpg" alt="">
     </div>
     <div>
         <h2 class="text-gray-700 font-bold text-5xl pb-4">
-            Learn how to write Laravel code
+            {{ $post->title }}
         </h2>
         <span class="text-gray-500">
             By <span class="font-bold italic text-gray-800">
-                Code With Dary
-            </span>,1 day ago
+                {{ $post->user->name }}
+            </span>Created on {{ date('jS M Y',strtotime($post->updated_at)) }}
         </span>
         <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+            {{ $post->description }}
         </p>
-        <a href="" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl"> Keep Reading</a>
+        <a href="/blog/{{ $post->slug }}" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl"> Keep Reading</a>
     </div>
 </div>
     
+@endforeach
+
+
 @endsection
